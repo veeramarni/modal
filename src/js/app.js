0fe5fc8a749c8a2731951ab224e4e24c3cce0bc2
nikhildaga <nikhil.daga.bitsian@gmail.com>
2016-03-16 20:23:44 +0530

    fix erros

--- src/js/app.js
+++ src/js/app.js
@@ -83,13 +83,9 @@ var MainViewController = React.createClass({
 
 				</div>
 				<div ref="panel">
-					<UI.Tabs.Navigator>
-						<UI.Tabs.Tab onTap={this.slideToggle}>
-							<span className="Tabs-Icon Tabs-Icon--lists" />
-						</UI.Tabs.Tab>
-					</UI.Tabs.Navigator>
+					<UI.NavigationBar name="main" />
 					<ViewManager name="main" defaultView="tabs">
-						<View name="tabs" component={TabViewController} />
+						<View name="tabs" slideToggle={this.slideToggle} component={TabViewController} />
 					</ViewManager>
 				</div>
 			</Container>
@@ -154,6 +150,10 @@ var TabViewController = React.createClass({
 					<View name="transitions-target" component={require('./views/transitions-target')} />
 				</ViewManager>
 				<UI.Tabs.Navigator>
+					<UI.Tabs.Tab onTap={this.props.slideToggle}>
+						<span className="Tabs-Icon Tabs-Icon--lists" />
+						<UI.Tabs.Label>Lists</UI.Tabs.Label>
+					</UI.Tabs.Tab>
 					<UI.Tabs.Tab onTap={this.selectTab.bind(this, 'lists')} selected={selectedTabSpan === 'lists'}>
 						<span className="Tabs-Icon Tabs-Icon--lists" />
 						<UI.Tabs.Label>Lists</UI.Tabs.Label>
